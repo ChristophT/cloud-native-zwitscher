d4ca5b5f49256f6ed01ac8a8b293e63884077dcd
ChristophT <thelen.christoph@gmail.com>
2017-12-04 23:33:07 +0100

    Upgrade to Spring Boot 2.0.0 and Gradle 4.2
    Unfinished, not all subproject dependencies updated yet

--- zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
+++ zwitscher-edge/src/test/java/de/qaware/cloud/nativ/zwitscher/edge/ZwitscherEdgeApplicationTests.java
@@ -25,13 +25,13 @@ package de.qaware.cloud.nativ.zwitscher.edge;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ZwitscherEdgeApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @WebAppConfiguration
 @TestPropertySource("classpath:/application-test.properties")
 public class ZwitscherEdgeApplicationTests {
